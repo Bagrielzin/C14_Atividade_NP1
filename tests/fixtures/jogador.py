55f49ebd28bbfeeb1e99273761cf8f96bd437e10
mariaclaraig <mclara.ignacio@gmail.com>
2026-04-10 12:25:46 -0300

    test: adiciona fixture de adversário

--- tests/fixtures/jogador.py
+++ tests/fixtures/jogador.py
@@ -20,3 +20,12 @@ def jogador_registrado_sem_deck(pokemons_mock):
     j.colecao = pokemons_mock[:6]
     j.deck = []
     return j
+
+@pytest.fixture
+def jogador_adversario(pokemons_mock):
+    j = Jogador(nome="Brook")
+    j.registro = True
+    j.colecao = pokemons_mock[:6]
+    j.deck = [pokemons_mock[3], pokemons_mock[4], pokemons_mock[5]] # pedra, corda, papel
+    return j
+ 
