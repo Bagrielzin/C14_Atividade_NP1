0a8681dc1802b8f9796e5f021356577118ce8b49
MatheusNetto1 <netto.matheus123@outlook.com>
2026-04-10 02:41:48 -0300

    test: adiciona testes para pokemon

--- tests/unit/services/test_pokemon.py
+++ tests/unit/services/test_pokemon.py
@@ -3,15 +3,21 @@ import pytest
 from unittest.mock import patch, mock_open, MagicMock
 from app.services.pokemon_service import get_all_pokemons
 
+# ========================
+# CENÁRIOS DE FLUXO NORMAL
+# ========================
 @patch("builtins.open", new_callable=mock_open, read_data='[{"id": 1, "original_name": "Bulbasaur", "name": "Bulbasaur", "move": "tesoura", "description": "Seed"}]')
 def test_get_all_pokemons_sucesso(mock_file):
-    """11. Testa leitura do arquivo de pokemons com sucesso"""
+    """Testa leitura do arquivo de pokemons com sucesso"""
     pokemons = get_all_pokemons()
     assert len(pokemons) == 1
     assert pokemons[0].name == "Bulbasaur"
 
+# ====================
+# CENÁRIOS DE EXTENSÃO
+# ====================
 @patch("builtins.open", side_effect=FileNotFoundError)
 def test_get_all_pokemons_arquivo_nao_encontrado(mock_file):
-    """12. Garante que se o json não existir, retorna lista vazia (conforme sua implementação)"""
+    """Garante que se o json não existir, retorna lista vazia (conforme sua implementação)"""
     pokemons = get_all_pokemons()
     assert pokemons == []
